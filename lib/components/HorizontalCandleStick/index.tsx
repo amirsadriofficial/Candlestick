14df512a21bf3b535633481f4f33b1ed8c0e1e9f
Amir Sadri <amirsadriofficial@gmail.com>
2024-09-03 23:23:07 +0330

    [REF]: move inline styles to css module

--- lib/components/HorizontalCandleStick/index.tsx
+++ lib/components/HorizontalCandleStick/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { HorizontalCandleStickProps } from "./types";
 import usePriceCalculations from "./hooks";
-// import DynamicTooltip from '../customize-tooltip';
+import styles from "./styles.module.css";
 
 export function HorizontalCandleStick({
   firstPrice,
@@ -24,7 +24,6 @@ export function HorizontalCandleStick({
     wickX,
     upPosition,
     downPosition,
-    middlePosition,
   } = usePriceCalculations({
     firstPrice,
     lastPrice,
@@ -38,9 +37,7 @@ export function HorizontalCandleStick({
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     const rect = e.currentTarget.getBoundingClientRect();
     const x = e.clientX - rect.left;
-    const value = Math.round(
-      (x / rect.width) * rangeTotal + lowerThreshold
-    );
+    const value = Math.round((x / rect.width) * rangeTotal + lowerThreshold);
     const percentageValue: number | string =
       ((value - prevDayPrice) / prevDayPrice) * 100;
     setHoverValue(value);
@@ -51,128 +48,56 @@ export function HorizontalCandleStick({
     setHoverValue(null);
     setPercentageValue(null);
   };
-  console.log("candleX", wickWidth, wickX);
+  console.log("position", upPosition, downPosition);
 
   return (
-    <div
-      style={{
-        height: "2.5rem",
-        position: "relative",
-        zIndex: 1000,
-        width: 1000,
-      }}
-    >
+    <div className={styles.container}>
       <div
-        style={{
-          position: "relative",
-          height: "0.125rem",
-          border: `0.0625rem solid #ddd`,
-          background: "lightgray",
-          margin: "1.25rem 0",
-          cursor: "pointer",
-          zIndex: 1200,
-          // overflow: "hidden",
-        }}
+        className={styles.candle_container}
         onMouseMove={handleMouseMove}
         onMouseLeave={handleMouseLeave}
       >
-        {/* candle body */}
+        {/* DESC: Candle body */}
         <div
           style={{
-            top: "50%",
             width: `${candleWidth}%`,
             left: `${candleX}%`,
-            transform: "translateY(-50%)",
             backgroundColor: lastPrice > firstPrice ? "green" : "red",
-            position: "absolute",
-            borderRadius: "0.625rem",
-            height: "16px",
-            zIndex: 1400,
-            transition: "width 0.5s ease, left 0.5s ease",
           }}
+          className={styles.candle_body}
         />
-        {/* candle dome */}
+        {/* DESC: Candle shadow: wick and tail */}
         <div
           style={{
-            position: "absolute",
-            top: "50%",
-            height: "0.5rem",
-            borderRadius: "0.625rem",
             width: `${wickWidth}%`,
             left: `${wickX > 0 ? wickX : 0}%`,
-            transform: "translateY(-50%)",
             backgroundColor: lastPrice > firstPrice ? "green" : "red",
-            zIndex: 1900,
-            transition: "width 0.5s ease, left 0.5s ease",
           }}
+          className={styles.candle_shadow}
         />
       </div>
-      {/* Middle Line */}
-      <div
-        style={{
-          width: "0.0625rem",
-          borderLeft: `0.0625rem dashed #000`,
-          height: "80%",
-          position: "absolute",
-          transform: ` translateX(-50%)`,
-          top: "20%",
-          left: `${middlePosition}%`,
-          transformOrigin: "center center",
-        }}
-      />
-      {/* Hover */}
+      {/* DESC: Middle line */}
+      <div className={styles.container_middle_line} />
+      {/* DESC: Hover */}
       {hoverValue !== null && percentageValue !== null && (
         <div
+          dir="ltr"
           style={{
             left: `${tooltipPosition.x}px`,
             top: `${tooltipPosition.y - 30}px`,
-            position: "absolute",
-            background: "#999",
-            padding: "0.1875rem 0.4375rem",
-            borderRadius: "0.4375rem",
-            color: "black",
-            display: "flex",
-            gap: "0.3125rem",
-            zIndex: 30000,
           }}
+          className={styles.tooltip}
         >
           <p
             style={{
-              fontSize: "0.875rem",
-              direction: "ltr",
               color: `${percentageValue < 0 ? "red" : "green"}`,
             }}
           >
-            ({percentageValue.toFixed(2)}%)
+            ({percentageValue?.toFixed(2)}%)
           </p>
-          <p style={{ fontSize: "0.875rem" }}>{hoverValue}</p>
+          <p>{hoverValue}</p>
         </div>
       )}
-      {/* <DynamicTooltip
-        title={`اخرین: ${LastTradePrice}`}
-        position={upPosition}
-        iconName={'chevron-down-full'}
-        placement="top"
-      />
-      <DynamicTooltip
-        title={`پایانی: ${data.downPointer}`}
-        position={downPosition}
-        iconName={'chevron-down-full'}
-        placement="bottom"
-        rotate={true}
-      /> */}
     </div>
   );
 }
-
-// import styles from "./styles.module.css";
-// export function HorizontalCandleStick(
-//   props: React.HTMLAttributes<HTMLElement>
-// ) {
-//   const { className, ...restProps } = props;
-//   return (
-//     <div className={`${className} ${styles.container}`} {...restProps}>
-//       Hello Horizontal
-//     </div>
-//   );
-// }
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { HorizontalCandleStick } from "../lib/main";
 // import { VerticalCandleStick } from "../lib/main";
+
 function App() {
   return (
     <div style={{ padding: "16px" }}>
-      {/* <p>this is demo page</p> */}
       <HorizontalCandleStick
         firstPrice={150.00}
-        lastPrice={155.00}
-        highestPrice={160.00}
+        lastPrice={160.00}
+        highestPrice={162.00}
         lowestPrice={145.00}
         upperThreshold={165.00}
         lowerThreshold={140.00}
